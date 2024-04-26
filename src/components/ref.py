f9cb830e7f8f293fa4d4c1fcb3162ab1cdd86e7a
Elias Brunila <brunilaelias@gmail.com>
2024-04-26 13:53:15 +0300

    ref luokka aluille

---
+++ src/components/ref.py
@@ -0,0 +1,19 @@
+class Ref:
+    def __init__(self, author, title, journal, year, volume, pages):
+        self.author: list = author 
+        self.title = title
+        self.journal = journal
+        self.year = year
+        self.volume = volume
+        self.pages = pages
+
+    def __str__(self):
+        s = ""
+        for i, name in enumerate(self.author):
+            
+            s += name
+            if (i < len(self.author) - 1):
+                s += ", "
+            else:
+                s += ". "
+        return s + self.title + ": " + self.journal + ", " +  self.volume + ". " + self.pages + ", " + str(self.year)
