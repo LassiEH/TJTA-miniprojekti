9c8c5a7957b84bdebbc2ccd5776176b277b303f6
Samuli Peura <samulipeura00@gmail.com>
2024-04-30 11:12:43 +0300

    lisätty testi ref luokan str metodille

--- src/components/tests/ref_test.py
+++ src/components/tests/ref_test.py
@@ -4,7 +4,16 @@ from ref import Ref
 class TestRef(unittest.TestCase):
 
     def setUp(self):
-        self.ref1 = Ref("Test Author", "Test Article", "Test Journal", 2020, 3, 5)
+        self.ref1 = Ref(["Author1", "Author2"], "Test Article", "Test Journal", 2020, 3, 5)
 
     def test_constructor(self):
-        self.assertEqual("Test Author", self.ref1.author)
+        self.assertEqual(["Author1", "Author2"], self.ref1.author)
+        self.assertEqual("Test Article", self.ref1.title)
+        self.assertEqual("Test Journal", self.ref1.journal)
+        self.assertEqual(2020, self.ref1.year)
+        self.assertEqual(3, self.ref1.volume)
+        self.assertEqual(5, self.ref1.pages)
+
+    def test_string_representation(self):
+        expected_output = "Author1, Author2. Test Article: Test Journal, 3. 5, 2020"
+        self.assertEqual(expected_output, str(self.ref1))
