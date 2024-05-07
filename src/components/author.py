df8efb5475e9d0874b81b6ea88ecf1832ab02bf4
Samuli Peura <137181608+sampex@users.noreply.github.com>
2024-05-07 11:50:18 +0300

    korjattu trailing-whitespace

--- src/components/author.py
+++ src/components/author.py
@@ -6,26 +6,26 @@ class Author:
     def __str__(self):
         if not self.firstname:
             return self.lastname
-        
+
         return f"{self.lastname} {self.firstname}"
-    
+
     def apastr(self):
         if not self.firstname:
             return self.lastname
-        
+
         retstr = f"{self.lastname}, "
         namelist = self.firstname.split()
         for n in namelist:
             #n.strip()
             retstr += n[0] + ". "
-        
+
         return retstr.strip()
-    
+
     def bibtexstr(self):
         if not self.firstname:
             return self.lastname
         #TODO: Poista välissä oleva whitespace joko tässä, konsturoijassa tai syötettä luettaessa
         return f"{self.lastname}, {self.firstname}"
-    
+
     def list_first_names(self):
         return self.firstname.split()
