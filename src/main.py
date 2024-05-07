536221ca9d10f84d32987a68b535b11922b0334a
Samuli Peura <137181608+sampex@users.noreply.github.com>
2024-05-07 11:40:45 +0300

    korjattu pylint virheet (wrong-import-order ja trailing-whitespace)

--- src/main.py
+++ src/main.py
@@ -1,17 +1,15 @@
+import sys
 from components.cli import Cli
 from components.poweruserUI import PoweruserUI
 from components.references import References
-import sys
 
 def main():
     if len(sys.argv) == 1:
         ui = Cli(References())
         ui.kaynnista()
         return
-    
+
     PoweruserUI(References(), sys.argv)
 
-    
-    
 if __name__ == "__main__":
     main()
