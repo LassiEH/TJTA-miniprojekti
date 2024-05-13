febe78a0016f1c0ed7e8dadfde43e8b007a5e8f2
LassiEH <lassi.haarala@live.fi>
2024-05-13 22:56:44 +0300

    Lisää testejä Cli-luokkaan

--- src/components/tests/cli_test.py
+++ src/components/tests/cli_test.py
@@ -5,3 +5,13 @@ from ..cli import Cli
 class TestCli(unittest.TestCase):
     def setUp(self):
         self.cli = Cli(lahteet=[])
+
+    @patch('builtins.input', side_effect=['1'])
+    def test_aloitus_add_source(self, mock_input):
+        with patch('builtins.print') as mock_print:
+            self.assertEqual(self.cli.aloitus(), 1)
+            
+    @patch('builtins.input', side_effect=['2'])
+    def test_aloitus_print_sources(self, mock_input):
+        with patch('builtins.print') as mock_print:
+            self.assertEqual(self.cli.aloitus(), 2)
